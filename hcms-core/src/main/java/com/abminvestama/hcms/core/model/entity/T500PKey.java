4897df5cda7a3116412faab57c355367a560b246
Yauri Attamimi <yauritux@gmail.com>
2015-12-16 11:13:40 +0700

    add company (T001) and personnel area (T500P) entities

---
+++ hcms-core/src/main/java/com/abminvestama/hcms/core/model/entity/T500PKey.java
@@ -0,0 +1,63 @@
+package com.abminvestama.hcms.core.model.entity;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author yauri (yauritux@gmail.com)
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ */
+public class T500PKey implements Serializable {
+
+	private static final long serialVersionUID = 6661653037461899358L;
+
+	private String persa;
+	
+	private String bukrs;
+	
+	public T500PKey() {}
+	
+	public T500PKey(String persa, String bukrs) {
+		this.persa = persa;
+		this.bukrs = bukrs;
+	}
+	
+	public String getPersa() {
+		return persa;
+	}
+	
+	public String getBukrs() {
+		return bukrs;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || o.getClass() != this.getClass()) {
+			return false;
+		}
+		
+		final T500PKey key = (T500PKey) o;
+		
+		if (persa != null ? !persa.equalsIgnoreCase(key.getPersa()) : key.persa != null) {
+			return false;
+		}
+		
+		if (bukrs != null ? !bukrs.equalsIgnoreCase(key.getBukrs()) : key.bukrs != null) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = persa != null ? persa.hashCode() : 0;
+		result = result * 31 + (bukrs != null ? bukrs.hashCode() : 0);
+		return result;
+	}
+}
